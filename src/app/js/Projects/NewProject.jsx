08e3ae77dcf5caea21df266bdb5cc2e4e442b0b4
gitgut0110 <jsimms0110@gmail.com>
2018-11-28 17:08:31 +0100

    activeProject added

--- src/app/js/Projects/NewProject.jsx
+++ src/app/js/Projects/NewProject.jsx
@@ -11,56 +11,96 @@ class NewProject extends React.Component {
       title: "",
       error: "",
       projects: null,
-      activeProject: null,
+      activeProject: null
     };
 
     this._handleInputChange = this._handleInputChange.bind(this);
     this._newProject = this._newProject.bind(this);
     this._fetchProjects = this._fetchProjects.bind(this);
     this._deleteItem = this._deleteItem.bind(this);
+    this._selectProject = this._selectProject.bind(this);
   }
 
   componentDidMount() {
     this._handleInputChange("title", "");
-    // this._fetchProjects();
+    this._fetchProjects();
   }
 
   render() {
+    let projectHeader;
+    this.state.activeProject ?  projectHeader = (
+      <div>
+        <h1>Active Project: {this.state.activeProject.title}</h1>
+        <button
+          onClick={() => {
+            this.setState({ activeProject: null });
+          }}
+        >
+          Change Project
+        </button>
+      </div>
+    ) :  projectHeader = null
+
+
     let projects = this.state.projects;
     let mappedProjects;
     if (projects !== null) {
       mappedProjects = projects.map(el => {
-        return <li key={el.title}>{el.title}>
-        <button onClick={()=>{this.setState({activeProject: el._id})}}>Select This ONe!</button>
-        <button onClick={()=>{this._deleteItem(`Project`,el._id)}}>Delete this Project</button>
-        </li>;
+        return (
+          <li key={el.title}>
+            {el.title}
+            <button
+              onClick={() => {
+                this._selectProject(el.id)
+              }}
+            >
+              Select This ONe!
+            </button>
+            <button
+              onClick={() => {
+                this._deleteItem(`Project`, el._id);
+              }}
+            >
+              Delete this Project
+            </button>
+          </li>
+        );
       });
     }
     return (
       <div className="container">
-      <h1>Active Project: {this.state.activeProject}</h1>
-        <h1>Start A New Project</h1>
-        <input
-          type="title"
-          value={this.state.title}
-          onChange={evt => this._handleInputChange("title", evt.target.value)}
-          className="input"
-          placeholder="myProject"
-        />
-        <br />
-        <br />
-        <button className="button" onClick={() => this._newProject()}>
-          Project Initiate!
-        </button>
-        <br />
-        <br />
-        <button onClick={() => this._fetchProjects()}>fetch projects</button>
-        {this.state.projects && <ol>{mappedProjects}</ol>}
-        <p>{this.props.error}</p>
-        <div className="separator" />
-        <Link className="link" to="/auth/sign-in">
-          Did you already start a project? Continue an existing one!
-        </Link>
+        {this.state.activeProject ? (
+          projectHeader
+        ) : (
+          <div>
+            <h1>Start A New Project</h1>
+            <input
+              type="title"
+              value={this.state.title}
+              onChange={evt =>
+                this._handleInputChange("title", evt.target.value)
+              }
+              className="input"
+              placeholder="myProject"
+            />
+            <br />
+            <br />
+            <button className="button" onClick={() => this._newProject()}>
+              Project Initiate!
+            </button>
+            <br />
+            <br />
+            <button onClick={() => this._fetchProjects()}>
+              fetch projects
+            </button>
+            {this.state.projects && <ol>{mappedProjects}</ol>}
+            <p>{this.props.error}</p>
+            <div className="separator" />
+            <Link className="link" to="/auth/sign-in">
+              Did you already start a project? Continue an existing one!
+            </Link>
+          </div>
+        )}
       </div>
     );
   }
@@ -82,7 +122,7 @@ class NewProject extends React.Component {
         owner: this.props.userID
       })
       .then(data => {
-        this.setState({ projects: [...this.state.projects, data] })
+        this.setState({ projects: [...this.state.projects, data] });
       })
       .catch(err => {
         this.setState({
@@ -113,9 +153,9 @@ class NewProject extends React.Component {
       });
   }
 
-  _deleteItem(key,id){
+  _deleteItem(key, id) {
     if (confirm(`Delete this Project and all records!?!`)) {
-      console.log(`deleting a`,key,`id:`,id);
+      console.log(`deleting a`, key, `id:`, id);
       this.setState({
         error: "",
         title: ""
@@ -127,18 +167,24 @@ class NewProject extends React.Component {
           id
         })
         .then(data => {
-          console.log(data)
-        }).then(results => this._fetchProjects()
-        )
+          console.log(data);
+        })
+        .then(results => this._fetchProjects())
         .catch(err => {
           this.setState({
             error: err.description
           });
         });
-  } else {
+    } else {
       console.log(`Deletion Aborted`);
+    }
   }
-      
+
+
+  _selectProject(id){
+    let activeProject = this.state.projects.find(obj => obj.id == id);
+    console.log(activeProject)
+    this.setState({ activeProject})
   }
 }
 
